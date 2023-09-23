587cd57df5ff66a3a79607ad4f5ce097703410d5
Tao <3535855771@qq.com>
2023-09-23 17:06:33 +0800

    89 commit

--- src/com/baisee/Test.java
+++ src/com/baisee/Test.java
@@ -1,10 +1,12 @@
 package com.baisee;
 
-
+/**
+ * @Date：2023/9/23
+ * @Name: 義涛
+ */
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
-        System.out.println(1);
     }
 }
